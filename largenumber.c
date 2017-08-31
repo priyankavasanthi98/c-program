c0993ca0027197df7b05294643953c291f1ed4fe
priyankavasanthi98 <31505533+priyankavasanthi98@users.noreply.github.com>
2017-08-31 15:18:19 +0530

    Create largenumber.c

---
+++ largenumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the values of a&b");
+scanf("%d%d",&a&b);
+if(a>b)
+{
+printf("a is largest%d",&a);
+}
+else
+{
+printf("b is large%d",&b);
+}
+return0;
+}
